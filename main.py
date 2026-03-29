24926d2ed2ee08d81406cd96bb0c3a2da5115ac0
nagane_00 <tyuke2525@gmail.com>
2026-03-29 22:11:44 +0900

    day02_簡易cssまで

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 # 一人用マイクロブログ（SNSライク）アプリケーションの制作
 
-from datetime import datetime, timedelta, timezone
 from sqlalchemy.orm import joinedload
+from datetime import datetime, timedelta, timezone
 
 from flask import Flask, abort, jsonify, redirect, render_template, request, url_for
 from flask_migrate import Migrate
+import flask_login
 
 from dotenv import load_dotenv
 import os
 
-from models import Post, Tag, User, TimerSession, db
+from werkzeug.security import generate_password_hash, check_password_hash
 
+from models import db
+
+
+# ========================================================================
+
+
+
+
+# Flaskアプリケーションの作成と設定
 def create_app():
     app = Flask(__name__)
+
+    # ログインマネージャーの設定
+    # login_manager = flask_login.LoginManager()
+    # login_manager.init_app(app)
+    # これ書くと動かねえ
+
 
     # 環境変数の読み込み
     load_dotenv()
@@ -34,8 +50,10 @@ app = create_app()
 # メイン画面
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # IDとパスワードでユーザー認証
 
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     # アプリの起動
