a5d328bab758772cbf6275f84af7a657c3f3c5cb
nagane00 <tyuke2525@gmail.com>
2026-04-09 16:07:47 +0900

    260409

--- routes.py
+++ routes.py
@@ -1,19 +1,30 @@
-# # routes.py
-# from flask import request, redirect, url_for, Blueprint
-# from models import db, Auth
+# routes.py
+from flask import request, redirect, url_for, Blueprint, render_template
+from sqlalchemy.orm import joinedload
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+import sqlite3
+from models import db, Auth
 
-# bp = Blueprint('main', __name__)
+bp = Blueprint('main', __name__)
 
-# @bp.route('/register', methods=['POST'])
-# def register():
-#     username = request.form['username']
-#     password = request.form['password']
+# パスワードハッシュ化のためのライブラリ
+from werkzeug.security import generate_password_hash    
 
-#     user = Auth(Auth_userid=username, Auth_password_hash=password)
-#     db.session.add(user)
-#     db.session.commit()
 
-# どういう役割のファイルかようわからんくなってきた
-# 必要になったときにもう一回調べる
+def hash_password(password):
+    return generate_password_hash(password)
+
+
+@bp.route('/register', methods=['POST'])
+def register():
+    username = 'a'
+    password = hash_password('a')  # ハッシュ化処理
+
+    user = Auth(Auth_userid=username, Auth_password_hash=password)
+    db.session.add(user)
+    db.session.commit()
+
+
 
 
